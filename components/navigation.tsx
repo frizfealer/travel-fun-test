d848988a36edccf3205516b6d05ef7026b7985ec
frizfealer <duke3d.harn@gmail.com>
2025-02-03 22:22:21 -0500

    add use client

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import Link from "next/link"
-import { useItinerary } from "@/contexts/ItineraryContext"
+import { useItinerary } from "../contexts/ItineraryContext"
 interface NavigationProps {
   activeTab: "explore" | "schedule"
 }
